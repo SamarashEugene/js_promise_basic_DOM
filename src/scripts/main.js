62c7194c09f226e796a480b9656fcf2f2d00ae49
Samarash Eugene <samarasha95@gmail.com>
2025-03-23 12:20:23 +0200

    Some changes were implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,36 @@
 const logo = document.querySelector('.logo');
 const body = document.querySelector('body');
 
-const prom1 = new Promise(
-  (resolve) => {
-    const div = document.createElement('div');
+const prom1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
 
-    div.classList.add('message');
-    div.textContent = 'Promise was resolved!';
-    // resolve(body.appendChild(div));
+prom1.then(() => {
+  const div = document.createElement('div');
 
-    logo.addEventListener('click', () => {
-      prom1.resolve(body.appendChild(div));
-    });
-  },
-  { once: true },
-);
+  div.classList.add('message');
+  div.textContent = 'Promise was resolved!';
+  body.appendChild(div);
+});
 
 const prom2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const div = document.createElement('div');
-
-    div.classList.add('message');
-    div.classList.add('error-message');
-    div.textContent = 'Promise was rejected!';
-
-    body.appendChild(div);
-
     reject(new Error('Promise was rejected!'));
   }, 3000);
 });
 
-prom2.catch((e) => {});
+function handleError() {
+  const div2 = document.createElement('div');
+
+  div2.classList.add('message');
+  div2.classList.add('error-message');
+  div2.textContent = 'Promise was rejected!';
+
+  body.appendChild(div2);
+}
+
+prom2.catch((error) => {
+  handleError(error);
+});
